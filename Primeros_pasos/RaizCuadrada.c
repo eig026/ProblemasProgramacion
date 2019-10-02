bf23d696995e391221847b3bd1f8f8a58972be9d
jrgarciaUAL <56065353+jrgarciaUAL@users.noreply.github.com>
2019-10-02 17:01:42 +0200

    Update RaizCuadrada.c

--- Primeros_pasos/RaizCuadrada.c
+++ Primeros_pasos/RaizCuadrada.c
@@ -1,21 +1,22 @@
 /*
- * @authors Equipo docente programacion grado de ingenieria 
+ * @authors Equipo docente programacion grado de ingenieria
  * @university  UAL
  * @date 2019-02-06
- 
-  Este programa muestra el resultado de la ecuacion 3x^2 + 4y^2
- 
+
+  Este programa muestra el resultado de la expresion (3x^2 + 4y^2)^(1/2)
+
  */
 
 #include<stdio.h>
 #include<math.h>
 
-main(void)
-{
-  int x,y;
-  float resultado;
-  printf("ingrese el valor de x, tambien y \n");
-  scanf("%d %d",&x,&y);
-  resultado=sqrt(3*x*x+4*y*y);
-  printf("%f",resultado);
+int main(void){
+    float x,y;
+    float resultado;
+
+    printf("Introduzca los valores de x e y separados por un espacio en blanco\n");
+    scanf(" %f %f",&x,&y);
+    resultado=sqrt(3*x*x+4*y*y);
+    printf("Resultado expresion: %f\n",resultado);
+    return 0;
 }
