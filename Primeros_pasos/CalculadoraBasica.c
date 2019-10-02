3348ce6cb488c1ceef6f15023527fe34ea0d3c82
jrgarciaUAL <56065353+jrgarciaUAL@users.noreply.github.com>
2019-10-02 17:12:09 +0200

    Update CalculadoraBasica.c

--- Primeros_pasos/CalculadoraBasica.c
+++ Primeros_pasos/CalculadoraBasica.c
@@ -1,21 +1,23 @@
 /*
- * @authors Equipo docente programacion grado de ingenieria 
+ * @authors Equipo docente programacion grado de ingenieria
  * @university  UAL
  * @date 2019-02-06
- 
-  Este programa saludara al usuario incorporando su nombre y apellidos al saludo
- 
+
+  Este programa realiza calculos aritmeticos sencillos con operandos enteros
+
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 int main(){
 	int a, b;
 	char operacion, c;
-	
+
 	do{ system("cls");
 		printf("Realizaremos una operacion aritmetica simple:\n\n");
-		printf("Introducir el primer valor con el que operar: ");
+		printf("Introduzca el primer valor con el que operar: ");
 		scanf(" %d",&a);
 		printf("Introduzca el segundo valor con el que operar: ");
 		scanf(" %d",&b);
@@ -23,17 +25,17 @@ int main(){
 		printf("Introduzca + para sumar, - para restar o * para multiplicar: ");
 		scanf(" %c",&operacion);
 		switch(operacion){
-	  		case '+': printf("El resultado es: %d\n", a+b);
-		 		break;
- 	  		case '-': printf("El resultado es: %d\n", a-b);
-                 		break;
-	  		case '*': printf("El resultado es: %d\n", a*b);
-                 		break;
-	  		default:  printf("Operacion incorrecta\n");
-	          		break;
+        case '+':   printf("El resultado es: %d\n", a+b);
+                    break;
+        case '-':   printf("El resultado es: %d\n", a-b);
+                    break;
+        case '*':   printf("El resultado es: %d\n", a*b);
+                    break;
+        default:    printf("Operacion incorrecta\n");
+                    break;
 		}
 	   	printf("\nDesea efectuar una nueva operacion (S/N)? ");
 	   	c=toupper(getch());
 	}while (c!='N');
-return 0;
+    return 0;
 }
