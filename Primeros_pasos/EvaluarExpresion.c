23c7449989c431455b7b522ef37482e4536519fd
jrgarciaUAL <56065353+jrgarciaUAL@users.noreply.github.com>
2019-10-02 16:50:31 +0200

    Update EvaluarExpresion.c

--- Primeros_pasos/EvaluarExpresion.c
+++ Primeros_pasos/EvaluarExpresion.c
@@ -1,26 +1,24 @@
 /*
- * @authors Equipo docente programacion grado de ingenieria 
+ * @authors Equipo docente programacion grado de ingenieria
  * @university  UAL
  * @date 2019-02-06
- 
-  Genera el llll
- 
+
+  Calcular una expresion aritmetica sencilla
+
  */
 
-#include<stdio.h>
-main(void)
-{
-    float resultado,x,w;
-  
+#include <stdio.h>
+
+int main(void){
+    float resultado,x,y;
+
     //se escriben dos valores separados por la barra espaciadora
-    printf("ingrese el valor de x , tambien de w \n");
-  
-    //se leen o capturan dos valores escritos en las variables x y w.
-    scanf("%f;%f",&x,&w);
-  
+    printf("Introduzca los valores de x e y separados por un espacio en blanco \n");
+    //se leen o capturan dos valores escritos en las variables x e y.
+    scanf(" %f %f",&x,&y);
     //Se evalua la expresion y el resultado es asignado a la variable resultado.
-    resultado=(x+w)/(2*w);
-  
+    resultado=(x+y)/(2*y);
     //se imprime el valor de la variable resultado.
-    printf("%f",resultado);
+    printf("Resultado expresion: %f",resultado);
+    return 0;
 }
