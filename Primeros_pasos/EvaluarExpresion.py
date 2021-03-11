37359c96ea0f50a73cb9007dcee3efce03a07a7f
imaguila <imaguila@ual.es>
2021-03-11 16:25:41 +0100

    1

---
+++ Primeros_pasos/EvaluarExpresion.py
@@ -0,0 +1,9 @@
+#se escriben dos valores separados por la barra espaciadora
+print("Introduzca los valores de x e y separados por salto de pagina \n");
+#se leen o capturan dos valores escritos en las variables x e y.
+x=float(input())
+y=float(input())
+#Se evalua la expresion y el resultado es asignado a la variable resultado.
+resultado=(x+y)/(2*y)
+#se imprime el valor de la variable resultado.
+print(f"Resultado expresion: {resultado}");
