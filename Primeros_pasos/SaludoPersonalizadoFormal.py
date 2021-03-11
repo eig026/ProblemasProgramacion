1408e43534701ac471d253ed468d822ee745ceb4
imaguila <imaguila@ual.es>
2021-03-11 16:12:00 +0100

    1

---
+++ Primeros_pasos/SaludoPersonalizadoFormal.py
@@ -0,0 +1,6 @@
+
+print("Por favor, introduzca su nombre: ")
+nombre = input()
+print("Por favor, introduzca sus apellidos:")
+apellidos = input()
+print(f"Me alegro de conocerle, {nombre} {apellidos}")
