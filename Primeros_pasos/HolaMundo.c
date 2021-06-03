d5360bd06250d1ddbb9a9d53bb4c190be8d909b7
imaguila <imaguila@ual.es>
2021-06-03 18:27:36 +0200

    Update HolaMundo.c

--- Primeros_pasos/HolaMundo.c
+++ Primeros_pasos/HolaMundo.c
@@ -1,7 +1,7 @@
 /*
- * @authors Equipo docente Programacion
- * @project Creacion de Materiales Didácticos en la Univer. de Almería (2021-2022)
- * @degree Grados en Ingeniería Eléctrica, Electrónica Industrial, Mecanica y Quimica Industrial
+ * @authors Equipo docente Programación
+ * @project Creación de Materiales Didácticos en la Univer. de Almería (2021-2022)
+ * Grados en Ingeniería Eléctrica, Electrónica Industrial, Mecánica y Química Industrial
  * @date 2021-02-06
 
   Programa que saluda educadamente al usuario mostrando  el mensaje Hola Mundo
