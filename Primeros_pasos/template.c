ee460c91da036c7e0b748218b4d3ea9841c4bc36
imaguila <imaguila@ual.es>
2021-11-13 17:07:19 +0000

    n

--- Primeros_pasos/template.c
+++ Primeros_pasos/template.c
@@ -1,3 +1,9 @@
+/*
+ * @authors Equipo docente Programación
+ * @project Creación de Materiales Didácticos en la Univer. de Almería (2021-2022)
+ * Grados en Ingeniería Elécctrica, Electrónica Industrial, Mecénica y Química industrial
+ * @date 2021-02-06
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
@@ -6,14 +12,13 @@
 int main(){
 	char c;
 
-	do{  system("cls");
+	do{
 			printf("\n");
 			printf("======\n\n");
 
 
 			printf("\n\nDesea efectuar una nueva operacion (s/n)? ");
-			fflush(stdin);
-			c=toupper(getchar());
+			scanf(" %c",&c);
 	}while (c!='N');
 	return 0;
 }
